dfd8821813186a3f8fd37a3f80956f4ecfa2592b
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-08-05 11:34:23 +1200

    Capitalised the variables

--- guess the number game.py
+++ guess the number game.py
@@ -2,23 +2,23 @@
 import random
 
 print('Hi, what is your name?')
-name = input()
+NAME = input()
 
-print('Hello there ' + name + ', I am thinking of a number between 1 and 20.')
-secnum = random.randint(1, 20)
+print('Hello there ' + NAME + ', I am thinking of a number between 1 and 20.')
+NUMBER = random.randint(1, 20)
 
-for guessesTaken in range(1, 7):
+for GUESSES_TAKEN in range(1, 7):
     print('Take a guess.')
-    guess = int(input())
+    GUESS = int(input())
 
-    if guess < secnum:
+    if GUESS < NUMBER:
         print('Too low! Shoot higher!')
-    elif guess > secnum:
+    elif GUESS > NUMBER:
         print('Too high! Try again!')
     else:
         break #breaks loop on accurate guess
 
-if guess == secnum:
-    print('Congratulations ' + name + '! You took ' + str(guessesTaken) + ' guesses to reach my number!')
+if GUESS == NUMBER:
+    print('Congratulations ' + NAME + '! You took ' + str(GUESSES_TAKEN) + ' guesses to reach my number!')
 else:
-    print('Sorry ' + name + ' , you ran out of guesses and the correct answer was ' + str(secnum))
+    print('Sorry ' + NAME + ' , you ran out of guesses and the correct answer was ' + str(NUMBER))
