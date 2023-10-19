18ac76e7fe32dcbfc0d5fac78c19449ffc74a622
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-10-19 19:28:28 +1300

    revision of old lessons, testing old code

--- character counter.py
+++ character counter.py
@@ -1,8 +1,8 @@
 import pprint
-MESSAGE =  'I like to ride my bicycle I like to ride my biiiiike'
+MESSAGE =  'This script counts occurrences of each character. 122333444455555.'
 COUNT = {}
 
-for CHARACTER in message.upper():
+for CHARACTER in MESSAGE.upper():
     COUNT.setdefault(CHARACTER, 0)
     COUNT[CHARACTER] = COUNT[CHARACTER] + 1
 
